4c02ec4554584cce0ebb66c53f4f2ab2e0e1a4f4
Uday Kiran <uday@ceooftech.com>
2024-10-27 14:23:51 +0530

    statements.c file added

---
+++ statements.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  printf("Have a good day!");
+  return 0;
+}
+
+/*Output:
+
+Hello World!Have a good day!*/
+
+/*From the example above, we have three statements:
+
+1. printf("Hello World!");
+2. printf("Have a good day!");
+3. return 0;
+
+The first statement is executed first (print "Hello World!" to the screen).
+Then the second statement is executed (print "Have a good day!" to the screen).
+And at last, the third statement is executed (end the C program successfully).
+
+You will learn more about statements while reading this tutorial.
+For now, just remember to always end them with a semicolon to avoid any errors.
+
+Coming up: The next chapter will teach you how to control the output and how to insert new lines to make it more readable.*/
